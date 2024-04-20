ddb9440b94281403f8d52ab643668045d1344668
darian <xxy.moming@gmail.com>
2024-04-20 08:41:02 +0800

    Initial Commit

---
+++ Welcome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Welcome
+//
+//  Created by Darian Mitchell  on 2024/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Welcome/WelcomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeApp.swift
+//  Welcome
+//
+//  Created by Darian Mitchell  on 2024/4/20.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
